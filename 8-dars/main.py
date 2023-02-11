fc7a5eea8081bd37f48d7a9d69c6c0c93d5bcf51
Husniddin <uzbekfreedom@gmail.com>
2023-02-11 13:42:07 +0500

    first task

---
+++ 8-dars/main.py
@@ -0,0 +1,10 @@
+kitoblar = {
+    "Joanna Rouling":{
+        "1": "garri potter" ,
+        "2": "Shimolga sayohat" ,
+    },
+    
+    "Antuan de Sent Ekzyuperi": "Kichkina shahzoda"
+}
+
+print(kitoblar["Joanna Rouling"]["1"])
