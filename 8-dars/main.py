01d09f620e31e37432aa210bbcf0800480c160be
Husniddin <uzbekfreedom@gmail.com>
2023-02-11 15:01:56 +0500

    kitoblar

--- 8-dars/main.py
+++ 8-dars/main.py
@@ -1,4 +1,17 @@
-son = 0
-while son<10:
-    son+=1
-    print('salom')
+kitoblar = {}
+
+belgi = True
+
+while belgi:
+    
+    kitob = input(f"Kitob nomi: ")
+    muallif = input(f"Muallif ismi:  ")
+    
+    kitoblar[muallif] = kitob  # lug'atga element qo'shish
+    
+    javob = input("yana kitob qo'shamizmi? (ha/y, yo'q/n  ") 
+    
+    if javob == "n":
+        belgi = False
+
+
