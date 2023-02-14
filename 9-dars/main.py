cdf0d0138c2818a434675e280f5bab0491c38321
Husniddin <uzbekfreedom@gmail.com>
2023-02-14 15:34:00 +0500

    books_info

--- 9-dars/main.py
+++ 9-dars/main.py
@@ -1,11 +1,17 @@
-def info(*info):
+# def book_info(name, author, genre, rel_data, lang, rang='oq',*etc):
     
-    """ bu funksiya to'liq ma'lumot berish uchun """
-    return info
+#     book = {
+#         'name': name,
+#         'author': author,
+#         'genre': genre,
+#         'rel_data':rel_data,
+#         'lang': lang,
+#         'rang':rang,
+#         "boshqa": etc
+#     }
+    
+#     return book
 
+# info = book_info('Mehrobdan chayon','Abdulla Qodiriy','drama','1929','uzbek','red','maslahat')
 
-fullname = info('ism','familiya')
-fullname2 = info('w3we','familiya')
-
-print(fullname)
-print(fullname2)
+# print(info.items())
