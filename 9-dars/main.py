5f84c3d2e395d27cdf50aa07cd9622d0f377c5de
Husniddin <uzbekfreedom@gmail.com>
2023-02-14 15:40:06 +0500

    kwargs

--- 9-dars/main.py
+++ 9-dars/main.py
@@ -1,4 +1,4 @@
-# def book_info(name, author, genre, rel_data, lang, rang='oq',*etc):
+# def book_info(name, author, genre, rel_data, lang, rang='oq',*boshqa):
     
 #     book = {
 #         'name': name,
@@ -15,3 +15,11 @@
 # info = book_info('Mehrobdan chayon','Abdulla Qodiriy','drama','1929','uzbek','red','maslahat')
 
 # print(info.items())
+
+def info(**full):
+    
+    return full
+
+fullInfo = info(ism="husniddin",familiya="Mehmonov")
+
+print(type(fullInfo))
