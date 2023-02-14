f9194978ee94796434b7359118f65e0b807f85a3
Husniddin <uzbekfreedom@gmail.com>
2023-02-14 14:21:11 +0500

    args

---
+++ 9-dars/main.py
@@ -0,0 +1,11 @@
+def info(*info):
+    
+    """ bu funksiya to'liq ma'lumot berish uchun """
+    return info
+
+
+fullname = info('ism','familiya')
+fullname2 = info('w3we','familiya')
+
+print(fullname)
+print(fullname2)
