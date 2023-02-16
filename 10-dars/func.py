a6cc409106a4e1fbcf906d7b25419ca5c166d17e
Husniddin <uzbekfreedom@gmail.com>
2023-02-16 18:00:04 +0500

    dars

--- 10-dars/func.py
+++ 10-dars/func.py
@@ -1,4 +1,7 @@
 def book_info(name, author, genre, rel_data, lang, rang='oq',**etc):  
+    """
+        Bu funksiya orqali siz kitoblar uchun ma'lumotni olishingiz mumkin
+    """
     book = {
         'name': name,
         'author': author,
@@ -9,15 +12,7 @@ def book_info(name, author, genre, rel_data, lang, rang='oq',**etc):
         "boshqa": etc
     }
     
-    return book
+    print(book)
 
-info = book_info('Mehrobdan chayon','Abdulla Qodiriy','drama','1929','uzbek','red','maslahat')
+# info = book_info('Mehrobdan chayon','Abdulla Qodiriy','drama','1929','uzbek','red','maslahat')
 
-
-# def info(**full):
-    
-#     return full
-
-# fullInfo = info(ism="husniddin",familiya="Mehmonov")
-
-# print(type(fullInfo))
